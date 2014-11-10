891ef7555d9d7de5f0ab11fb6ddbef1e4bfb35ef
Anna Pione <annapione@gmail.com>
2014-11-10 11:37:13 -0600

    Homework submission

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -3,5 +3,42 @@ class PhotosController < ApplicationController
     @list_of_photos = Photo.all
   end
 
+  def show
+    @photo_number=params[:id]
+    i=Photo.find(@photo_number)
+    @photo_source=i.source
+    @photo_caption=i.caption
+  end
+
+  def new_form
+  end
+
+  def create_row
+    @new = Photo.new
+    @new.caption= params[:the_caption]
+    @new.source= params[:the_source]
+    @new.save
+  end
+
+  def destroy
+    @photo_number=params[:id]
+    i=Photo.find(@photo_number)
+    i.destroy
+  end
+
+  def edit_form
+    @photo_number=params[:id]
+    i=Photo.find(@photo_number)
+    @photo_source=i.source
+    @photo_caption=i.caption
+  end
+
+  def update_row
+    @photo_number=params[:id]
+    @edit=Photo.find(@photo_number)
+    @edit.caption= params[:the_caption]
+    @edit.source= params[:the_source]
+    @edit.save
+  end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141105190754) do
+ActiveRecord::Schema.define(version: 20141107002049) do
+
+  create_table "instructors", force: true do |t|
+    t.string   "first_name"
+    t.string   "last_name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "photos", force: true do |t|
     t.string   "source"
